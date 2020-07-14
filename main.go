d6a33b36de7732d07a2bb6f69a18bbec63bff650
Tadeo Kondrak <me@tadeo.ca>
2020-07-14 09:24:22 -0600

    Clean up main.go

--- main.go
+++ main.go
@@ -29,10 +29,13 @@ func runClient(conn net.Conn) {
 }
 
 func main() {
-	flag.BoolVar(&ircDebug, "ircdebug", false, "enable logging of all irc communication")
-	flag.BoolVar(&discordDebug, "discorddebug", false, "enable logging of some discord communication")
+	flag.BoolVar(&ircDebug, "ircdebug", false,
+		"enable logging of all irc communication")
+	flag.BoolVar(&discordDebug, "discorddebug", false,
+		"enable logging of some discord communication")
 	flag.BoolVar(&tlsEnabled, "tls", false, "enable tls encryption")
-	flag.IntVar(&port, "port", 0, "port to run on, defaults to 6667/6697 depending on tls")
+	flag.IntVar(&port, "port", 0,
+		"port to run on, defaults to 6667/6697 depending on tls")
 	flag.StringVar(&certfile, "cert", "", "tls certificate file")
 	flag.StringVar(&keyfile, "key", "", "tls key file")
 	flag.Parse()
@@ -40,31 +43,41 @@ func main() {
 	log.SetFlags(0)
 
 	var listener net.Listener
-	var err error
 	if tlsEnabled {
 		if certfile == "" || keyfile == "" {
 			log.Fatalf("certfile/keyfile required for tls")
 		}
+
 		var cert tls.Certificate
-		cert, err = tls.LoadX509KeyPair(certfile, keyfile)
+		cert, err := tls.LoadX509KeyPair(certfile, keyfile)
 		if err != nil {
 			log.Fatalf("failed to load keypair: %v", err)
 		}
+
 		config := tls.Config{
 			Certificates: []tls.Certificate{cert},
 		}
+
 		if port == 0 {
 			port = 6697
 		}
-		listener, err = tls.Listen("tcp", fmt.Sprintf(":%d", port), &config)
+
+		addr := fmt.Sprintf(":%d", port)
+
+		listener, err = tls.Listen("tcp", addr, &config)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
 	} else {
 		if port == 0 {
 			port = 6667
 		}
+
+		var err error
 		listener, err = net.Listen("tcp", fmt.Sprintf(":%d", port))
-	}
-	if err != nil {
-		log.Fatalf("failed to create listener: %v", err)
+		if err != nil {
+			log.Fatalf("failed to create listener: %v", err)
+		}
 	}
 	defer listener.Close()
 

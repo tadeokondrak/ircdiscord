93d81313b2328291e5a04717b96c31b0195487d8
Tadeo Kondrak <me@tadeo.ca>
2020-07-17 18:09:36 -0600

    main: Update to use package server

--- main.go
+++ main.go
@@ -6,46 +6,69 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
-	"github.com/tadeokondrak/ircdiscord/internal/client"
+	"github.com/pkg/errors"
+	"github.com/tadeokondrak/ircdiscord/internal/server"
 )
 
-var (
-	ircDebug     bool
-	discordDebug bool
-	tlsEnabled   bool
-	port         int
-	certfile     string
-	keyfile      string
-)
-
-func runClient(conn net.Conn) {
-	cl := client.New(conn, ircDebug, discordDebug)
-	defer cl.Close()
-
-	if err := cl.Run(); err != nil {
-		log.Println(err)
+func listen(port int) (net.Listener, error) {
+	if port == 0 {
+		port = 6667
 	}
+	addr := fmt.Sprintf(":%d", port)
+
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, errors.Wrap(err,
+			"failed to create listener")
+	}
+
+	return listener, nil
 }
 
-func runServer(ln net.Listener) {
-	defer ln.Close()
-
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Fatalf("failed to accept connection: %v", err)
-		}
-
-		go runClient(conn)
+func listenTLS(port int, certfile, keyfile string) (net.Listener, error) {
+	if certfile == "" || keyfile == "" {
+		return nil, errors.New(
+			"certfile and keyfile are required for TLS")
 	}
+
+	cert, err := tls.LoadX509KeyPair(certfile, keyfile)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load keypair")
+	}
+
+	config := &tls.Config{Certificates: []tls.Certificate{cert}}
+
+	if port == 0 {
+		port = 6697
+	}
+	addr := fmt.Sprintf(":%d", port)
+
+	listener, err := tls.Listen("tcp", addr, config)
+	if err != nil {
+		return nil, errors.Wrap(err,
+			"failed to create listener")
+	}
+
+	return listener, nil
 }
 
 func main() {
+	var (
+		ircDebug     bool
+		discordDebug bool
+		tlsEnabled   bool
+		port         int
+		certfile     string
+		keyfile      string
+	)
+
 	flag.BoolVar(&ircDebug, "ircdebug", false,
-		"enable logging of all irc communication")
+		"enable logging of irc communication")
 	flag.BoolVar(&discordDebug, "discorddebug", false,
-		"enable logging of some discord communication")
+		"enable logging of discord communication")
 	flag.BoolVar(&tlsEnabled, "tls", false, "enable tls encryption")
 	flag.IntVar(&port, "port", 0,
 		"port to run on, defaults to 6667/6697 depending on tls")
@@ -55,40 +78,39 @@ func main() {
 
 	log.SetFlags(0)
 
-	if tlsEnabled {
-		if certfile == "" || keyfile == "" {
-			log.Fatalf("certfile and keyfile are required for TLS")
-		}
-
-		cert, err := tls.LoadX509KeyPair(certfile, keyfile)
+	var ln net.Listener
+	if !tlsEnabled {
+		var err error
+		ln, err = listen(port)
 		if err != nil {
-			log.Fatalf("failed to load keypair: %v", err)
+			log.Fatalln(err)
 		}
-
-		config := &tls.Config{Certificates: []tls.Certificate{cert}}
-
-		if port == 0 {
-			port = 6697
-		}
-		addr := fmt.Sprintf(":%d", port)
-
-		listener, err := tls.Listen("tcp", addr, config)
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
-
-		runServer(listener)
 	} else {
-		if port == 0 {
-			port = 6667
-		}
-		addr := fmt.Sprintf(":%d", port)
-
-		listener, err := net.Listen("tcp", addr)
+		var err error
+		ln, err = listenTLS(port, certfile, keyfile)
 		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
+			log.Fatalln(err)
 		}
+	}
 
-		runServer(listener)
+	server := server.New(ln)
+	defer server.Close()
+
+	errors := make(chan error)
+
+	go func() {
+		if err := server.Run(); err != nil {
+			errors <- err
+		}
+	}()
+
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, os.Interrupt)
+
+	select {
+	case err := <-errors:
+		log.Println(err)
+	case sig := <-sigch:
+		log.Printf("received signal %v, exiting", sig)
 	}
 }
